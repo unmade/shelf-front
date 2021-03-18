e43402cefa6227199262b18603401e9daaeae9d8
Aleksei Maslakov <lesha.maslakov@gmail.com>
2021-03-19 00:02:25 +0300

    Close Dropdown on click

--- src/components/ui/Breadcrumb/BreadcrumbDropdown.jsx
+++ src/components/ui/Breadcrumb/BreadcrumbDropdown.jsx
@@ -6,15 +6,13 @@ import Dropdown from '../Dropdown';
 function BreadcrumbDropdown({ children, items, itemRender: Render }) {
   return (
     <Dropdown
-      overlay={() => (
+      overlay={({ closeOverlay }) => (
         <div className="max-w-xs">
           <div className="m-2 sm:m-0 p-2 bg-white flex flex-col rounded shadow space-y-1">
             {(items.map((item) => (
-              <Render
-                key={item.path}
-                name={item.name}
-                path={item.path}
-              />
+              <span key={item.path} onClick={closeOverlay} aria-hidden>
+                <Render name={item.name} path={item.path} />
+              </span>
             )))}
           </div>
         </div>
