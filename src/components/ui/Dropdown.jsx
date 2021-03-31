c349b2ac2133c11dcee93a3cf089a3f9f18c7bae
Aleksei Maslakov <lesha.maslakov@gmail.com>
2021-04-01 00:54:51 +0300

    Open/hide dropdown with animation

--- src/components/ui/Dropdown.jsx
+++ src/components/ui/Dropdown.jsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { Transition } from '@headlessui/react';
 import { createPopper } from '@popperjs/core';
 
 class Dropdown extends React.Component {
@@ -32,21 +33,21 @@ class Dropdown extends React.Component {
     document.removeEventListener('mouseup', this.handleClickOutside);
   }
 
+  handleClickOutside(event) {
+    const shouldClose = (
+      this.triggerRef && !this.triggerRef.current.contains(event.target)
+      && this.popoverRef && !this.popoverRef.current.contains(event.target)
+    );
+    if (shouldClose) {
+      this.closePopover();
+    }
+  }
+
   setPopoverVisible(value) {
     this.setState({ popoverVisible: value });
   }
 
   openPopover() {
-    if (!this.popper) {
-      createPopper(
-        this.triggerRef.current,
-        this.popoverRef.current,
-        {
-          placement: 'bottom-start',
-        },
-      );
-      document.addEventListener('mouseup', this.handleClickOutside);
-    }
     this.setPopoverVisible(true);
   }
 
@@ -62,16 +63,6 @@ class Dropdown extends React.Component {
       this.closePopover();
     } else {
       this.openPopover();
-    }
-  }
-
-  handleClickOutside(event) {
-    const shouldClose = (
-      this.triggerRef && !this.triggerRef.current.contains(event.target)
-      && this.popoverRef && !this.popoverRef.current.contains(event.target)
-    );
-    if (shouldClose) {
-      this.closePopover();
     }
   }
 
@@ -91,10 +82,30 @@ class Dropdown extends React.Component {
           ref={this.popoverRef}
           className="absolute z-50"
         >
-          {(popoverVisible) && (
-            // eslint-disable-next-line react/jsx-props-no-spreading
+          <Transition
+            show={popoverVisible}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+            afterEnter={() => {
+              if (this.popper === null || this.popper === undefined) {
+                createPopper(
+                  this.triggerRef.current,
+                  this.popoverRef.current,
+                  {
+                    placement: 'bottom-start',
+                  },
+                );
+                document.addEventListener('mouseup', this.handleClickOutside);
+              }
+            }}
+          >
+            {/* eslint-disable-next-line react/jsx-props-no-spreading */}
             <Overlay closeOverlay={this.closePopover} {...overlayProps} />
-          )}
+          </Transition>
         </div>
       </>
     );
