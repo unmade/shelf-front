aee5f6994f9830f9d88caa277379e7a7c4bfa9e3
Aleksei Maslakov <lesha.maslakov@gmail.com>
2021-04-06 23:45:36 +0300

    Exclude file to be moved from MoveDialog

--- src/containers/FolderPicker.js
+++ src/containers/FolderPicker.js
@@ -5,9 +5,22 @@ import { getFilesByPath } from '../store/reducers/files';
 import FolderPicker from '../components/FolderPicker';
 import { listFolder } from '../store/actions/files';
 
+function getFilesByPathExclude(state, props) {
+  const { path, excludeId } = props;
+  const files = getFilesByPath(state, path);
+  if (excludeId === null || excludeId === undefined) {
+    return files;
+  }
+  const nextFiles = files.filter((fileId) => fileId !== excludeId);
+  if (files.length === nextFiles.length) {
+    return files; // return object from state to prevent re-renders
+  }
+  return nextFiles;
+}
+
 export default connect(
   (state, ownProps) => ({
-    items: getFilesByPath(state, ownProps.path),
+    items: getFilesByPathExclude(state, ownProps),
   }),
   {
     listFolder,
