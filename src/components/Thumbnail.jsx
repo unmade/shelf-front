a16f777159444e77f93a91611ccf2019abd33e50
Aleksei Maslakov <lesha.maslakov@gmail.com>
2021-06-28 23:55:19 +0300

    Refactor Thumbnail to use hooks

--- src/components/Thumbnail.jsx
+++ src/components/Thumbnail.jsx
@@ -1,50 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { useDispatch, useSelector } from 'react-redux';
+
+import { fetchThumbnail } from '../store/actions/files';
+
+import { getFileById, getThumbnailById } from '../store/reducers/files';
+
 import FileIcon from './FileIcon';
 
-function Thumbnail({
-  className, deferred, file, size, thumbs, fetchThumbnail,
-}) {
-  const { id, path } = file;
-  const hasThumbnail = (thumbs !== null && thumbs[size] !== null && thumbs[size] !== undefined);
-  React.useEffect(() => {
-    if (!hasThumbnail && !deferred) {
-      fetchThumbnail(id, path, size);
-    }
-  }, [id, path, size, deferred, hasThumbnail, fetchThumbnail]);
+function Thumbnail({ className, deferred, fileId, size }) {
+  const dispatch = useDispatch();
 
-  if (!hasThumbnail) {
-    return <FileIcon className={className} mediatype={file.mediatype} hidden={file.hidden} />;
+  const file = useSelector((state) => getFileById(state, fileId));
+  const thumbs = useSelector((state) => getThumbnailById(state, file.id));
+
+  const loaded = (thumbs != null && thumbs[size] != null && thumbs[size]);
+  const shouldLoad = (file.has_thumbnail && !loaded && !deferred);
+
+  const { id, path } = file;
+
+  React.useEffect(() => {
+    if (shouldLoad) {
+      dispatch(fetchThumbnail(id, path, size));
+    }
+  }, [id, path, size, deferred, shouldLoad, dispatch]);
+
+  if (file.has_thumbnail && loaded) {
+    return <img className={`object-contain ${className}`} src={thumbs[size]} alt={file.name} />;
   }
-  return <img className={`object-contain ${className}`} src={thumbs[size]} alt={file.name} />;
+  return <FileIcon className={className} mediatype={file.mediatype} hidden={file.hidden} />;
 }
 
 Thumbnail.propTypes = {
   className: PropTypes.string,
   deferred: PropTypes.bool,
-  file: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    path: PropTypes.string.isRequired,
-    mediatype: PropTypes.string.isRequired,
-  }).isRequired,
+  fileId: PropTypes.string.isRequired,
   size: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
-  thumbs: PropTypes.shape({
-    xs: PropTypes.string,
-    sm: PropTypes.string,
-    md: PropTypes.string,
-    lg: PropTypes.string,
-    xl: PropTypes.string,
-  }),
-  fetchThumbnail: PropTypes.func.isRequired,
 };
 
 Thumbnail.defaultProps = {
   className: '',
   deferred: false,
   size: 'xs',
-  thumbs: null,
 };
 
 export default Thumbnail;
