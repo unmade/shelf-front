694447031f9be0b2d0ecb5b1db74a8bd22477601
Aleksei Maslakov <lesha.maslakov@gmail.com>
2021-09-17 01:31:16 +0300

    Setup eslint and prettier (#40)

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/prop-types': 0,
+    'object-curly-newline': [
+      'error',
+      {
+        ObjectExpression: {
+          minProperties: 5,
+          multiline: true,
+          consistent: true,
+        },
+        ObjectPattern: {
+          minProperties: 5,
+          multiline: true,
+          consistent: true,
+        },
+        ImportDeclaration: {
+          minProperties: 4,
+          multiline: true,
+          consistent: true,
+        },
+        ExportDeclaration: {
+          minProperties: 4,
+          multiline: true,
+          consistent: true,
+        },
+      },
+    ],
+  },
+};
