3771c9e0e17b287e5db112ba8dad1b553fa1f100
Aleksei Maslakov <lesha.maslakov@gmail.com>
2021-09-18 12:20:46 +0300

    Proper handle h-screen on mobile devices (#42)

--- src/store/api.js
+++ src/store/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.REACT_APP_API_BASE_URL || 'http://192.168.1.9:8000';
+const API_BASE_URL = import.meta.env.SNOWPACK_PUBLIC_API_BASE_URL ?? 'http://localhost:8000';
 
 export default API_BASE_URL;
 
