b00ec68d952d2c0e825a2de570fbf6a5e60b701f
Aleksei Maslakov <lesha.maslakov@gmail.com>
2022-05-19 00:40:01 +0300

    Use redux-toolkit in store/loading (#73)

--- src/store/reducers/loading.js
+++ src/store/reducers/loading.js
@@ -1,85 +1,51 @@
+import { createReducer } from '@reduxjs/toolkit';
 import * as authActions from '../actions/auth';
 import { types as fileTypes } from '../actions/files';
 import { types as userTypes } from '../actions/users';
 import { scopes, types } from '../actions/loading';
 
-function loading(state = {}, action) {
-  switch (action.type) {
-    case types.SET_LOADING: {
-      const { scope, value } = action.payload;
-      return {
-        ...state,
-        [scope]: value,
-      };
-    }
-    case authActions.issueTokenFulfilled.type:
-    case authActions.issueTokenRejected.type:
-      return {
-        ...state,
-        [scopes.signingIn]: false,
-      };
-    case userTypes.ADD_BOOKMARK_FAILURE:
-    case userTypes.ADD_BOOKMARK_SUCCESS:
-    case userTypes.REMOVE_BOOKMARK_FAILURE:
-    case userTypes.REMOVE_BOOKMARK_SUCCESS:
-      return {
-        ...state,
-        [scopes.bookmarking]: false,
-      };
-    case fileTypes.CREATE_FOLDER_FAILURE:
-    case fileTypes.CREATE_FOLDER_SUCCESS:
-      return {
-        ...state,
-        [scopes.creatingFolder]: false,
-      };
-    case fileTypes.DELETE_IMMEDIATELY_FAILURE:
-    case fileTypes.DELETE_IMMEDIATELY_SUCCESS:
-      return {
-        ...state,
-        [scopes.deletingFileImmediately]: false,
-      };
-    case fileTypes.EMPTY_TRASH_FAILURE:
-    case fileTypes.EMPTY_TRASH_SUCCESS:
-      return {
-        ...state,
-        [scopes.emptyingTrash]: false,
-      };
-    case fileTypes.FIND_DUPLICATES_FAILURE:
-    case fileTypes.FIND_DUPLICATES_SUCCESS:
-      return {
-        ...state,
-        [scopes.searchingDuplicates]: false,
-      };
-    case fileTypes.LIST_FOLDER_FAILURE:
-    case fileTypes.LIST_FOLDER_SUCCESS:
-      return {
-        ...state,
-        [scopes.listingFolder]: false,
-      };
-    case fileTypes.MOVE_FILE_FAILURE:
-    case fileTypes.MOVE_FILE_SUCCESS:
-    case fileTypes.MOVE_FILE_BATCH_FAILURE:
-    case fileTypes.MOVE_FILE_BATCH_SUCCESS:
-      return {
-        ...state,
-        [scopes.movingFile]: false,
-      };
-    case fileTypes.MOVE_TO_TRASH_FAILURE:
-    case fileTypes.MOVE_TO_TRASH_SUCCESS:
-      return {
-        ...state,
-        [scopes.movingToTrash]: false,
-      };
-    case userTypes.LIST_BOOKMARKS_FAILURE:
-    case userTypes.LIST_BOOKMARKS_SUCCESS:
-      return {
-        ...state,
-        [scopes.listingBookmarks]: false,
-      };
-    default:
-      return state;
-  }
+const INITIAL_STATE = {};
+
+const scopeByType = {
+  [authActions.issueTokenFulfilled.type]: scopes.signingIn,
+  [authActions.issueTokenRejected.type]: scopes.signingIn,
+  [userTypes.ADD_BOOKMARK_FAILURE]: scopes.bookmarking,
+  [userTypes.ADD_BOOKMARK_SUCCESS]: scopes.bookmarking,
+  [userTypes.REMOVE_BOOKMARK_FAILURE]: scopes.bookmarking,
+  [userTypes.REMOVE_BOOKMARK_SUCCESS]: scopes.bookmarking,
+  [fileTypes.CREATE_FOLDER_FAILURE]: scopes.creatingFolder,
+  [fileTypes.CREATE_FOLDER_SUCCESS]: scopes.creatingFolder,
+  [fileTypes.DELETE_IMMEDIATELY_FAILURE]: scopes.deletingFileImmediately,
+  [fileTypes.DELETE_IMMEDIATELY_SUCCESS]: scopes.deletingFileImmediately,
+  [fileTypes.EMPTY_TRASH_FAILURE]: scopes.emptyingTrash,
+  [fileTypes.EMPTY_TRASH_SUCCESS]: scopes.emptyingTrash,
+  [fileTypes.FIND_DUPLICATES_FAILURE]: scopes.searchingDuplicates,
+  [fileTypes.FIND_DUPLICATES_SUCCESS]: scopes.searchingDuplicates,
+  [fileTypes.LIST_FOLDER_FAILURE]: scopes.listingFolder,
+  [fileTypes.LIST_FOLDER_SUCCESS]: scopes.listingFolder,
+  [fileTypes.MOVE_FILE_FAILURE]: scopes.movingFile,
+  [fileTypes.MOVE_FILE_SUCCESS]: scopes.movingFile,
+  [fileTypes.MOVE_FILE_BATCH_FAILURE]: scopes.movingFile,
+  [fileTypes.MOVE_FILE_BATCH_SUCCESS]: scopes.movingFile,
+  [fileTypes.MOVE_TO_TRASH_BATCH_FAILURE]: scopes.movingToTrash,
+  [fileTypes.MOVE_TO_TRASH_BATCH_SUCCESS]: scopes.movingToTrash,
+  [fileTypes.LIST_BOOKMARKS_FAILURE]: scopes.listingBookmarks,
+  [fileTypes.LIST_BOOKMARKS_SUCCESS]: scopes.listingBookmarks,
+};
+
+function isLoaded(action) {
+  return scopeByType[action.type] != null;
 }
+
+const loading = createReducer(INITIAL_STATE, (builder) => {
+  builder.addCase(types.SET_LOADING, (state, action) => {
+    const { scope, value } = action.payload;
+    state[scope] = value;
+  });
+  builder.addMatcher(isLoaded, (state, action) => {
+    state[scopeByType[action.type]] = false;
+  });
+});
 
 export default loading;
 
