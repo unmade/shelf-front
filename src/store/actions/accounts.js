7843840a4d16c50c8ba5aeb97818325c76ee7016
Aleksei Maslakov <lesha.maslakov@gmail.com>
2022-05-23 20:30:37 +0300

    Refactor accounts store (#73)

--- src/store/actions/accounts.js
+++ src/store/actions/accounts.js
@@ -1,33 +1,7 @@
-export const types = {
-  LIST_ACCOUNTS: 'LIST_ACCOUNTS',
-  LIST_ACCOUNTS_SUCCESS: 'LIST_ACCOUNTS_SUCCESS',
+import { createAction } from '@reduxjs/toolkit';
 
-  RETRIEVE_CURRENT_ACCOUNT: 'RETRIEVE_CURRENT_ACCOUNT',
-  RETRIEVE_CURRENT_ACCOUNT_SUCCESS: 'RETRIEVE_CURRENT_ACCOUNT_SUCCESS',
-  RETRIEVE_CURRENT_ACCOUNT_FAILURE: 'RETRIEVE_CURRENT_ACCOUNT_FAILURE',
-};
-
-export const listAccounts = (page, perPage = 25) => ({
-  type: types.LIST_ACCOUNTS,
+export const listAccounts = createAction('accounts/listAccounts', (page, perPage = 25) => ({
   payload: { page, perPage },
-});
+}));
 
-export const listAccountsSuccess = (data) => ({
-  type: types.LIST_ACCOUNTS_SUCCESS,
-  payload: { ...data },
-});
-
-export const retrieveCurrentAccount = () => ({
-  type: types.RETRIEVE_CURRENT_ACCOUNT,
-  payload: null,
-});
-
-export const retrieveCurrentAccountSuccess = (account) => ({
-  type: types.RETRIEVE_CURRENT_ACCOUNT_SUCCESS,
-  payload: { account },
-});
-
-export const retrieveCurrentAccountFailure = (err) => ({
-  type: types.RETRIEVE_CURRENT_ACCOUNT_FAILURE,
-  payload: { err },
-});
+export const retrieveCurrentAccount = createAction('accounts/retrieveCurrentAccount');
