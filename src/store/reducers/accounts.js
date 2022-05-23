28f27bf903e4f6c96cb79910c42661226e814689
Aleksei Maslakov <lesha.maslakov@gmail.com>
2022-05-23 20:30:37 +0300

    Refactor accounts store (#73)

--- src/store/reducers/accounts.js
+++ src/store/reducers/accounts.js
@@ -1,74 +1,52 @@
-import { combineReducers } from '@reduxjs/toolkit';
+import { combineReducers, createReducer } from '@reduxjs/toolkit';
 
-import { types } from '../actions/accounts';
+import { fulfilled, rejected } from '../actions';
+import * as actions from '../actions/accounts';
 
-function normalize(items) {
-  const data = {};
-  items.forEach((item) => {
-    data[item.id] = item;
+const BY_ID_INITIAL_STATE = {};
+
+const byId = createReducer(BY_ID_INITIAL_STATE, (builder) => {
+  builder.addCase(fulfilled(actions.listAccounts), (state, action) => {
+    const { results } = action.payload;
+    results.forEach((account) => {
+      state[account.id] = account;
+    });
   });
-  return data;
-}
-
-function accountsById(state = {}, action) {
-  switch (action.type) {
-    case types.LIST_ACCOUNTS_SUCCESS: {
-      const { results } = action.payload;
-      return {
-        ...state,
-        ...normalize(results),
-      };
-    }
-    case types.RETRIEVE_CURRENT_ACCOUNT_SUCCESS: {
-      const { account } = action.payload;
-      return {
-        ...state,
-        [account.id]: account,
-      };
-    }
-    default:
-      return state;
-  }
-}
-
-function accountIdsByPage(state = {}, action) {
-  switch (action.type) {
-    case types.LIST_ACCOUNTS_SUCCESS: {
-      const { results, page } = action.payload;
-      return {
-        ...state,
-        [page]: results.map((account) => account.id),
-      };
-    }
-    default:
-      return state;
-  }
-}
-
-function currentAccountId(state = null, action) {
-  switch (action.type) {
-    case types.RETRIEVE_CURRENT_ACCOUNT_SUCCESS: {
-      const { account } = action.payload;
-      return account.id;
-    }
-    case types.RETRIEVE_CURRENT_ACCOUNT_FAILURE: {
-      return null;
-    }
-    default:
-      return state;
-  }
-}
-
-export default combineReducers({
-  byId: accountsById,
-  byPage: accountIdsByPage,
-  currentAccountId,
+  builder.addCase(fulfilled(actions.retrieveCurrentAccount), (state, action) => {
+    const account = action.payload;
+    state[account.id] = account;
+  });
 });
 
-export const getAccountById = (state, props) => state.accounts.byId[props.id];
+const CURRENT_ACCOUNT_INITIAL_STATE = null;
+
+const currentAccountId = createReducer(null, (builder) => {
+  builder.addCase(fulfilled(actions.retrieveCurrentAccount), (_, action) => {
+    const account = action.payload;
+    return account.id;
+  });
+  builder.addCase(rejected(actions.retrieveCurrentAccount), () => CURRENT_ACCOUNT_INITIAL_STATE);
+});
+
+const IDS_BY_PAGE_INITIAL_STATE = {};
+
+const idsByPage = createReducer(IDS_BY_PAGE_INITIAL_STATE, (builder) => {
+  builder.addCase(fulfilled(actions.listAccounts), (state, action) => {
+    const { page, results } = action.payload;
+    state[page] = results.map((account) => account.id);
+  });
+});
+
+export default combineReducers({
+  byId,
+  currentAccountId,
+  idsByPage,
+});
+
+export const getAccountById = (state, { id }) => state.accounts.byId[id];
 export const getCurrentAccountId = (state) => state.accounts.currentAccountId;
 export const getCurrentAccount = (state) =>
   getAccountById(state, { id: [getCurrentAccountId(state)] });
 export const getIsCurrentAccountSuperuser = (state) => getCurrentAccount(state)?.superuser ?? false;
-export const getAccountsIdsByPage = (state, props) => state.accounts.byPage[props.page];
+export const getAccountsIdsByPage = (state, { page }) => state.accounts.idsByPage[page];
 export const isAdmin = (state) => getCurrentAccount(state)?.superuser ?? false;
--- src/store/sagas/accounts.js
+++ src/store/sagas/accounts.js
@@ -2,48 +2,33 @@ import { put, select, takeEvery } from 'redux-saga/effects';
 
 import * as api from '../api';
 import * as actions from '../actions/accounts';
+import { started, loaded } from '../actions/loading';
 import { getAccessToken } from '../reducers/auth';
 
-import { tryRequest, tryResponse } from './_try';
+import { tryFetch } from './_try';
 
-function* listAccounts({ payload }) {
+function* listAccounts({ type, payload }) {
   const accessToken = yield select(getAccessToken);
 
   const { page, perPage } = payload;
   const request = api.get(`/accounts/list_all?page=${page}&per_page=${perPage}`, accessToken);
-  const [response, err] = yield tryRequest(request);
-  if (err != null) {
-    return;
-  }
 
-  const [data, parseErr] = yield tryResponse(response.json());
-  if (parseErr != null) {
-    return;
-  }
-
-  yield put(actions.listAccountsSuccess(data));
+  yield put(started(type));
+  yield tryFetch(type, request);
+  yield put(loaded(type));
 }
 
-function* retrieveCurrentAccount() {
+function* retrieveCurrentAccount({ type }) {
   const accessToken = yield select(getAccessToken);
 
   const request = api.get('/accounts/get_current', accessToken);
-  const [response, err] = yield tryRequest(request);
-  if (err !== null) {
-    yield put(actions.retrieveCurrentAccountFailure(err));
-    return;
-  }
 
-  const [data, parseErr] = yield tryResponse(response.json());
-  if (parseErr !== null) {
-    yield put(actions.retrieveCurrentAccountFailure(err));
-    return;
-  }
-
-  yield put(actions.retrieveCurrentAccountSuccess(data));
+  yield put(started(type));
+  yield tryFetch(type, request);
+  yield put(loaded(type));
 }
 
 export default [
-  takeEvery(actions.types.LIST_ACCOUNTS, listAccounts),
-  takeEvery(actions.types.RETRIEVE_CURRENT_ACCOUNT, retrieveCurrentAccount),
+  takeEvery(actions.listAccounts, listAccounts),
+  takeEvery(actions.retrieveCurrentAccount, retrieveCurrentAccount),
 ];
