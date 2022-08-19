29d0be60074efe58c660d5da82e4838b1ee768a9
Aleksei Maslakov <lesha.maslakov@gmail.com>
2022-08-19 23:21:32 +0300

    Clear store on token expiration (#88)

--- src/components/RequireAuth.jsx
+++ src/components/RequireAuth.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom';
 
+import { signedOut } from '../store/actions/auth';
 import { getIsAuthenticated } from '../store/reducers/auth';
 
 export default function RequireAuth({ children, redirectTo }) {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const { pathname } = useLocation();
   const authenticated = useSelector(getIsAuthenticated);
 
   React.useEffect(() => {
     if (!authenticated) {
+      dispatch(signedOut());
       navigate(`${redirectTo}?next=${pathname}`);
     }
-  }, [authenticated, pathname, navigate]);
+  }, [authenticated, pathname, dispatch, navigate]);
 
   if (!authenticated) {
     return null;
