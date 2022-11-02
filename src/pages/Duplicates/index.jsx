5809031369ae1ca7e0810c818600bb1f7a8a8e69
Aleksei Maslakov <lesha.maslakov@gmail.com>
2022-11-02 23:43:34 +0300

    Hide Duplicates page on small screens (#103)

--- src/pages/Duplicates/index.jsx
+++ src/pages/Duplicates/index.jsx
@@ -1,38 +1,62 @@
 import React from 'react';
 
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 
 import { Dialogs } from '../../constants';
 import useDirPath from '../../hooks/dir-path';
 import * as icons from '../../icons';
 
+import Button from '../../components/ui/Button';
+
 import DeleteDialog from '../../components/DeleteDialog';
+
 import DuplicatesResult from './DuplicatesResult';
 import SelectFolderDialogButton from './SelectFolderDialogButton';
 
 function Duplicates() {
   const { t } = useTranslation('duplicates');
 
+  const navigate = useNavigate();
+
   const dirPath = useDirPath();
   const initialTargetFolderPath = dirPath !== '.' ? dirPath : null;
   const [targetFolderPath, setTargetFolderPath] = React.useState(initialTargetFolderPath);
 
+  const goBack = () => {
+    navigate(-1);
+  };
+
   if (targetFolderPath == null) {
     return (
-      <div className="flex h-full flex-col items-center justify-center">
-        <div>
-          <icons.DocumentSearchOutlined className="h-12 w-12 text-gray-400" />
+      <>
+        {/* desktop */}
+        <div className="hidden h-full flex-col items-center justify-center lg:flex">
+          <div>
+            <icons.DocumentSearchOutlined className="h-12 w-12 text-gray-400" />
+          </div>
+          <div className="mt-2">
+            <p className="text-center text-2xl font-medium">{t('duplicates:welcomeTitle')}</p>
+            <p className="mt-2 text-center">{t('duplicates:welcomeHelpText')}</p>
+          </div>
+          <div className="mt-4">
+            <SelectFolderDialogButton
+              type="primary"
+              dirPath="."
+              onSelectFolder={setTargetFolderPath}
+            >
+              {t('duplicates:selectFolderButton')}
+            </SelectFolderDialogButton>
+          </div>
         </div>
-        <div className="mt-2">
-          <p className="text-center text-2xl font-medium">{t('duplicates:welcomeTitle')}</p>
-          <p className="mt-2 text-center">{t('duplicates:welcomeHelpText')}</p>
+        {/* mobile */}
+        <div className="flex h-full flex-col items-center justify-center space-y-6 lg:hidden">
+          <p>{t('duplicates:pageNotAvailableOnMobile')}</p>
+          <Button onClick={goBack} type="primary">
+            {t('duplicates:goBack')}
+          </Button>
         </div>
-        <div className="mt-4">
-          <SelectFolderDialogButton type="primary" dirPath="." onSelectFolder={setTargetFolderPath}>
-            {t('duplicates:selectFolderButton')}
-          </SelectFolderDialogButton>
-        </div>
-      </div>
+      </>
     );
   }
 
