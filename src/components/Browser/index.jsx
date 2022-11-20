2dbe69d81706b5e51b8790cca74d31bbd4f75b69
Aleksei Maslakov <lesha.maslakov@gmail.com>
2022-11-21 00:30:51 +0300

    Update scroll position when offset changed

--- src/components/Browser/index.jsx
+++ src/components/Browser/index.jsx
@@ -17,68 +17,64 @@ import SidePreview from './SidePreview';
 import StatusBar from './StatusBar';
 import useSidePreview from '../../hooks/preview-available';
 
-const Browser = React.memo(
-  ({ actionButton, dirPath, droppable, emptyIcon, emptyTitle, emptyDescription }) => {
-    const isLaptop = useMediaQuery({ query: MediaQuery.lg });
-    const withSidePreview = useSidePreview();
-    const path = dirPath ?? '.';
+function Browser({ actionButton, dirPath, droppable, emptyIcon, emptyTitle, emptyDescription }) {
+  const isLaptop = useMediaQuery({ query: MediaQuery.lg });
+  const withSidePreview = useSidePreview();
+  const path = dirPath ?? '.';
 
-    const { data, isLoading: loading } = useListFolderQuery(path);
+  const { data, isLoading: loading } = useListFolderQuery(path);
 
-    const entities = React.useMemo(() => Object.values(data?.entities ?? {}), [data?.entities]);
+  const entities = React.useMemo(() => Object.values(data?.entities ?? {}), [data?.entities]);
 
-    if (loading) {
-      return null;
-    }
+  if (loading) {
+    return null;
+  }
 
-    const fileTableView = (
-      <FileTableView
-        items={entities}
-        loading={loading}
-        path={path}
-        scrollKey={path}
-        itemRender={FileTableCell}
-        emptyIcon={emptyIcon}
-        emptyTitle={emptyTitle}
-        emptyDescription={emptyDescription}
-      />
-    );
+  const fileTableView = (
+    <FileTableView
+      items={entities}
+      loading={loading}
+      path={path}
+      scrollKey={path}
+      itemRender={FileTableCell}
+      emptyIcon={emptyIcon}
+      emptyTitle={emptyTitle}
+      emptyDescription={emptyDescription}
+    />
+  );
 
-    const tableView = droppable ? (
-      <FileDrop
-        className="h-full"
-        uploadTo={path}
-        render={({ dragging }) => (
-          <div className="relative h-full w-full">
-            <div
-              className={`${dragging ? 'block' : 'hidden'} absolute z-10 h-full w-full px-2 pb-2`}
-            >
-              <div className="h-full w-full rounded-2xl border-4 border-dashed border-teal-200 dark:border-teal-600" />
-            </div>
-            {fileTableView}
+  const tableView = droppable ? (
+    <FileDrop
+      className="h-full"
+      uploadTo={path}
+      render={({ dragging }) => (
+        <div className="relative h-full w-full">
+          <div className={`${dragging ? 'block' : 'hidden'} absolute z-10 h-full w-full px-2 pb-2`}>
+            <div className="h-full w-full rounded-2xl border-4 border-dashed border-teal-200 dark:border-teal-600" />
+          </div>
+          {fileTableView}
+        </div>
+      )}
+    />
+  ) : (
+    fileTableView
+  );
+
+  return (
+    <div className="flex h-full flex-col">
+      <BrowserHeader isLaptop={isLaptop} actionButton={actionButton} />
+      <div className="flex h-full flex-row overflow-scroll pt-4">
+        <div className={`h-full ${withSidePreview ? 'w-7/12' : 'w-full'}`}>{tableView}</div>
+        {withSidePreview && (
+          <div className="w-5/12 overflow-scroll">
+            <SidePreview path={path} />
           </div>
         )}
-      />
-    ) : (
-      fileTableView
-    );
-
-    return (
-      <div className="flex h-full flex-col">
-        <BrowserHeader isLaptop={isLaptop} actionButton={actionButton} />
-        <div className="flex h-full flex-row overflow-scroll pt-4">
-          <div className={`h-full ${withSidePreview ? 'w-7/12' : 'w-full'}`}>{tableView}</div>
-          {withSidePreview && (
-            <div className="w-5/12 overflow-scroll">
-              <SidePreview path={path} />
-            </div>
-          )}
-        </div>
-        <StatusBar dirPath={path} isLaptop={isLaptop} />
       </div>
-    );
-  }
-);
+      <StatusBar dirPath={path} isLaptop={isLaptop} />
+    </div>
+  );
+}
 
 export default Browser;
 
