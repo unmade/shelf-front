1e64585ab7ed259777aa2890943f515bc96565f0
Aleksei Maslakov <lesha.maslakov@gmail.com>
2022-12-01 12:08:28 +0300

    Fix bug with selection in SidePreview

---
+++ src/components/Browser/BrowserContainer.jsx
@@ -0,0 +1,118 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { useDispatch } from 'react-redux';
+
+import useSidePreview from '../../hooks/preview-available';
+
+import { useListFolderQuery } from '../../store/files';
+import { fileBrowserPathChanged, fileSelectionCleared } from '../../store/browser';
+
+import { FileShape } from '../../types';
+
+import FileDrop from '../../containers/FileDrop';
+
+import FileTableCell from '../FileTableCell';
+import FileTableView from '../FileTableView';
+
+import SidePreview from './SidePreview';
+
+function FileTableViewContainer({
+  dirPath,
+  items,
+  loading,
+  droppable,
+  emptyIcon,
+  emptyTitle,
+  emptyDescription,
+}) {
+  const fileTableView = (
+    <FileTableView
+      items={items}
+      loading={loading}
+      path={dirPath}
+      scrollKey={dirPath}
+      itemRender={FileTableCell}
+      emptyIcon={emptyIcon}
+      emptyTitle={emptyTitle}
+      emptyDescription={emptyDescription}
+    />
+  );
+
+  return droppable ? (
+    <FileDrop
+      className="h-full"
+      uploadTo={dirPath}
+      render={({ dragging }) => (
+        <div className="relative h-full w-full">
+          <div className={`${dragging ? 'block' : 'hidden'} absolute z-10 h-full w-full px-2 pb-2`}>
+            <div className="h-full w-full rounded-2xl border-4 border-dashed border-teal-200 dark:border-teal-600" />
+          </div>
+          {fileTableView}
+        </div>
+      )}
+    />
+  ) : (
+    fileTableView
+  );
+}
+
+FileTableViewContainer.propTypes = {
+  dirPath: PropTypes.string.isRequired,
+  items: PropTypes.arrayOf(FileShape).isRequired,
+  loading: PropTypes.bool.isRequired,
+  droppable: PropTypes.bool.isRequired,
+  emptyIcon: PropTypes.element.isRequired,
+  emptyTitle: PropTypes.string.isRequired,
+  emptyDescription: PropTypes.string.isRequired,
+};
+
+function BrowserContainer({ dirPath, droppable, emptyIcon, emptyTitle, emptyDescription }) {
+  const dispatch = useDispatch();
+
+  const withSidePreview = useSidePreview();
+  const { data, isLoading: loading } = useListFolderQuery(dirPath);
+
+  const files = React.useMemo(
+    () => data?.ids.map((id) => data?.entities[id]) ?? [],
+    [data?.ids, data?.entities]
+  );
+
+  React.useEffect(() => {
+    dispatch(fileBrowserPathChanged({ path: dirPath }));
+    return () => {
+      dispatch(fileSelectionCleared());
+    };
+  }, [dirPath, dispatch]);
+
+  return (
+    <>
+      <div className={`h-full ${withSidePreview ? 'w-7/12' : 'w-full'}`}>
+        <FileTableViewContainer
+          dirPath={dirPath}
+          items={files}
+          loading={loading}
+          droppable={droppable}
+          emptyIcon={emptyIcon}
+          emptyTitle={emptyTitle}
+          emptyDescription={emptyDescription}
+        />
+      </div>
+      {withSidePreview && (
+        <div className="w-5/12 overflow-scroll">
+          <SidePreview itemsMap={data?.entities} />
+        </div>
+      )}
+    </>
+  );
+}
+
+BrowserContainer.propTypes = {
+  dirPath: PropTypes.string.isRequired,
+  droppable: PropTypes.bool.isRequired,
+  emptyIcon: PropTypes.element.isRequired,
+  emptyTitle: PropTypes.string.isRequired,
+  emptyDescription: PropTypes.string.isRequired,
+};
+
+export default BrowserContainer;
--- src/pages/Bookmarks.jsx
+++ src/pages/Bookmarks.jsx
@@ -23,15 +23,50 @@ import SidePreview from '../components/Browser/SidePreview';
 
 const empty = [];
 
-function Bookmarks() {
+function BookmarkHeader() {
   const { t } = useTranslation();
-  const dispatch = useDispatch();
-  const { data: fileIds } = useListBookmarksQuery();
-  const { data: files, isLoading } = useGetBatchQuery(fileIds, { skip: fileIds == null });
-  const withSidePreview = useSidePreview();
   const isLaptop = useMediaQuery({ query: MediaQuery.lg });
 
   const title = t('Bookmarks');
+
+  return (
+    <div className="flex flex-row items-center justify-between px-6 py-7 sm:pl-5 sm:pr-8">
+      {!isLaptop ? (
+        <>
+          <SideBarModal />
+          <h2 className="ml-2 truncate text-xl font-medium text-gray-900 dark:text-zinc-100 sm:text-3xl">
+            {title}
+          </h2>
+        </>
+      ) : (
+        <div className="flex min-w-0 flex-1 items-center ">
+          <icons.BookmarkOutlined className="ml-2 h-7 w-7 text-gray-400 dark:text-zinc-500" />
+          <h2 className="ml-2 truncate text-xl font-medium text-gray-900 dark:text-zinc-100 sm:text-3xl">
+            {title}
+          </h2>
+        </div>
+      )}
+      <div className="flex items-center text-2xl sm:ml-6 sm:space-x-8">
+        {/* hack: this is to center text like on other pages */}
+        <div className="h-9 w-9">&nbsp;</div>
+      </div>
+    </div>
+  );
+}
+
+BookmarkHeader.propTypes = {};
+
+function BookmarkList() {
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
+  const { data: fileIds } = useListBookmarksQuery();
+  const { data, isLoading } = useGetBatchQuery(fileIds, { skip: fileIds == null });
+  const withSidePreview = useSidePreview();
+
+  const files = React.useMemo(
+    () => data?.ids.map((id) => data?.entities[id]) ?? empty,
+    [data?.ids, data?.entities]
+  );
 
   React.useEffect(
     () => () => {
@@ -41,59 +76,46 @@ function Bookmarks() {
   );
 
   return (
+    <div className="flex flex-1 flex-row pt-4">
+      <div className={withSidePreview ? 'w-7/12' : 'w-full'}>
+        <FileTableView
+          className="border-transparent"
+          items={files}
+          scrollKey="bookmarks-table-view" // possible collissions
+          itemRender={FileTableCell}
+          loading={isLoading}
+          emptyIcon={
+            <icons.BookmarkAltOutlined className="h-12 w-12 text-gray-400 dark:text-zinc-500" />
+          }
+          emptyTitle={t('Bookmarks will appear here')}
+        />
+      </div>
+      {withSidePreview && (
+        <div className="w-5/12">
+          <SidePreview itemsMap={data.entities} />
+        </div>
+      )}
+    </div>
+  );
+}
+
+BookmarkList.propTypes = {};
+
+function Bookmarks() {
+  return (
     <DeleteDialogProvider>
       <MoveDialogProvider>
         <RenameFileDialogProvider>
           <div className="flex h-full flex-col">
-            {/* header */}
-            <div className="flex flex-row items-center justify-between px-6 py-7 sm:pl-5 sm:pr-8">
-              {!isLaptop ? (
-                <>
-                  <SideBarModal />
-                  <h2 className="ml-2 truncate text-xl font-medium text-gray-900 dark:text-zinc-100 sm:text-3xl">
-                    {title}
-                  </h2>
-                </>
-              ) : (
-                <div className="flex min-w-0 flex-1 items-center ">
-                  <icons.BookmarkOutlined className="ml-2 h-7 w-7 text-gray-400 dark:text-zinc-500" />
-                  <h2 className="ml-2 truncate text-xl font-medium text-gray-900 dark:text-zinc-100 sm:text-3xl">
-                    {title}
-                  </h2>
-                </div>
-              )}
-              <div className="flex items-center text-2xl sm:ml-6 sm:space-x-8">
-                {/* hack: this is to center text like on other pages */}
-                <div className="h-9 w-9">&nbsp;</div>
-              </div>
-            </div>
-
-            {/* bookmark list */}
-            <div className="flex flex-1 flex-row pt-4">
-              <div className={withSidePreview ? 'w-7/12' : 'w-full'}>
-                <FileTableView
-                  className="border-transparent"
-                  items={files ?? empty}
-                  scrollKey="bookmarks-table-view" // possible collissions
-                  itemRender={FileTableCell}
-                  loading={isLoading}
-                  emptyIcon={
-                    <icons.BookmarkAltOutlined className="h-12 w-12 text-gray-400 dark:text-zinc-500" />
-                  }
-                  emptyTitle={t('Bookmarks will appear here')}
-                />
-              </div>
-              {withSidePreview && (
-                <div className="w-5/12">
-                  <SidePreview />
-                </div>
-              )}
-            </div>
+            <BookmarkHeader />
+            <BookmarkList />
           </div>
         </RenameFileDialogProvider>
       </MoveDialogProvider>
     </DeleteDialogProvider>
   );
 }
+
+Bookmarks.propTypes = {};
 
 export default Bookmarks;
