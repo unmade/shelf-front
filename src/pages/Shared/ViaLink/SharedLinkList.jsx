3cb55fdea7600bbc212c85854c135a1e66fdab8e
Aleksei Maslakov <lesha.maslakov@gmail.com>
2023-12-07 01:39:21 +0300

    Optimistic update `getSharedLink` on `listFilesSharedViaLink`

--- src/pages/Shared/ViaLink/SharedLinkList.jsx
+++ src/pages/Shared/ViaLink/SharedLinkList.jsx
@@ -31,11 +31,11 @@ function SharedLinkList() {
   return (
     <div>
       {/* header */}
-      <div className="show-on-hover-trigger mb-1 flex flex-row items-center border-l border-r border-transparent px-9 py-2 text-xs font-medium uppercase tracking-wider text-gray-500 dark:text-zinc-400">
-        <div className="flex w-full md:w-3/5 lg:w-3/4">
+      <div className="mb-1 flex flex-row items-center border-l border-r border-transparent px-9 py-2 text-xs font-medium uppercase tracking-wider text-gray-500 dark:text-zinc-400">
+        <div className="flex w-full md:w-3/4">
           <div className="ml-3">{t('colName.title', { defaultValue: 'Name' })}</div>
         </div>
-        <div className="hidden items-center space-x-4 md:flex md:w-2/5 lg:w-1/4">
+        <div className="hidden items-center space-x-4 md:flex md:w-1/4">
           <div className="w-48 md:block text-left">
             {t('colDateCreated.title', { defaultValue: 'Date Created' })}
           </div>
