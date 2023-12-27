41a649c77ece96b8751d70662baca5eb503e4f6d
Aleksei Maslakov <lesha.maslakov@gmail.com>
2023-12-27 22:57:48 +0300

    Fine tune `CopyToClipboardButton` button

--- src/pages/Shared/ViaLink/SharedLinkListItem.jsx
+++ src/pages/Shared/ViaLink/SharedLinkListItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 
 import useSharedLink from '../../../hooks/shared-link';
@@ -20,6 +21,8 @@ import Thumbnail from '../../../components/Thumbnail';
 import TimeAgo from '../../../components/ui/TimeAgo';
 
 function SharedLinkListItem({ fileId }) {
+  const { t } = useTranslation('sharedLinkSetting');
+
   const item = useSelector((state) => selectFileSharedViaLinkById(state, fileId));
   const { name, path, token } = item;
   const sharedLink = useSharedLink({ token, filename: name });
@@ -29,7 +32,7 @@ function SharedLinkListItem({ fileId }) {
   const openCopyLinkDialog = useCopyLinkDialog();
 
   return (
-    <div className="show-on-hover-trigger even:ring-gray-50 even:bg-gray-50 even:dark:bg-zinc-700/30 px-12 h-[72px] flex items-center w-full rounded-xl">
+    <div className="group/row even:ring-gray-50 even:bg-gray-50 even:dark:bg-zinc-700/30 px-12 h-[72px] flex items-center w-full rounded-xl">
       {/* file icon and name */}
       <div className="md:w-3/4 flex text-gray-900 dark:text-zinc-100 w-full">
         <div className="flex w-full min-w-0 items-center space-x-3">
@@ -46,11 +49,16 @@ function SharedLinkListItem({ fileId }) {
           </span>
         </div>
         <div className="ml-2 flex items-center space-x-4">
-          <CopyToClipboardButton
-            className="show-on-hover-target"
-            text={sharedLink}
-            disabled={sharedLink == null}
-          />
+          <div className="min-w-max">
+            <CopyToClipboardButton
+              className="hidden md:group-hover/row:flex"
+              title={t('copyToClipboardBtn.title', { defaultValue: 'Copy to clipboard' })}
+              value={sharedLink}
+              disabled={sharedLink == null}
+            >
+              {t('copyToClipboardBtn.text', { defaultValue: 'Copy link' })}
+            </CopyToClipboardButton>
+          </div>
           <div className="flex items-center">
             <Button
               variant="text"
@@ -61,7 +69,7 @@ function SharedLinkListItem({ fileId }) {
         </div>
       </div>
 
-      <div className="ml-6 flex items-center text-left space-x-4 md:w-1/4">
+      <div className="ml-6 hidden md:flex items-center text-left space-x-4 md:w-1/4">
         <div className="w-48">
           <TimeAgo mtime={createdAt} />
         </div>
