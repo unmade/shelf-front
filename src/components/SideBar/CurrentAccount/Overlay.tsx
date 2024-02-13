d3f23b9f6ebd9dae47a32f06e18342f379dda283
Aleksei Maslakov <lesha.maslakov@gmail.com>
2024-02-13 22:01:39 +0300

    Show display name in SideBar

---
+++ src/components/SideBar/CurrentAccount/Overlay.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+
+import * as icons from 'icons';
+import * as routes from 'routes';
+import { useAppDispatch } from 'hooks';
+
+import { signedOut } from 'store/authSlice';
+
+import Avatar from 'components/ui/Avatar';
+import Button from 'components/ui/Button';
+
+import PreferredAppearance from './PreferredAppearance';
+
+interface Props {
+  fullName: string;
+  email: string | null;
+  username: string;
+}
+
+export default function Overlay({ fullName, email, username }: Props) {
+  const { t } = useTranslation();
+
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const displayName = fullName || email || username;
+
+  const onSignOut = () => {
+    navigate(routes.SIGNIN.route);
+    dispatch(signedOut());
+  };
+
+  return (
+    <div className="flex min-w-[12rem] flex-col space-y-2 rounded-xl bg-white p-2 shadow focus:outline-none dark:bg-zinc-800 dark:shadow-zinc-900/70">
+      <div className="flex flex-row items-center p-1">
+        <Avatar className="h-10 w-10 rounded-lg" username={displayName} />
+        <div className="ml-2 flex flex-1 flex-col text-left text-gray-700 dark:text-gray-300">
+          <div className="text-sm font-semibold">{displayName}</div>
+          {fullName && <div className="text-xs">{email ?? `@${username}`}</div>}
+        </div>
+      </div>
+      <hr className="dark:border-zinc-700" />
+      <PreferredAppearance />
+      <div className="space-y-1">
+        <Button
+          full
+          variant="text"
+          color="danger"
+          title={t('Sign Out')}
+          icon={<icons.LogoutOutlined className="h-5 w-5" />}
+          onClick={onSignOut}
+        >
+          <div className="my-1">{t('Sign Out')}</div>
+        </Button>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/SideBar/CurrentAccount/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { useGetCurrentAccountQuery } from 'store/accounts';
+
+import * as icons from 'icons';
+
+import Avatar from 'components/ui/Avatar';
+import Dropdown from 'components/ui/Dropdown';
+
+import Overlay from './Overlay';
+
+export default function CurrentAccount() {
+  const { account } = useGetCurrentAccountQuery(undefined, {
+    selectFromResult: ({ data }) => ({ account: data }),
+  });
+
+  if (!account) {
+    return null;
+  }
+
+  const { display_name: fullName, email, username } = account;
+  const displayName = fullName || username;
+
+  return (
+    <Dropdown
+      overlay={<Overlay fullName={fullName} email={email} username={username} />}
+      placement="top-end"
+    >
+      <div className="flex flex-row items-center lg:block xl:flex">
+        <Avatar username={displayName} className="h-10 w-10 shrink-0" />
+        <div className="ml-2 flex min-w-0 flex-1 flex-col text-left lg:hidden xl:flex">
+          <div className="truncate text-sm font-semibold text-gray-700 dark:text-zinc-400">
+            {displayName}
+          </div>
+        </div>
+        <div className="block lg:hidden xl:block">
+          <icons.Selector className="h-5 w-5 text-gray-500 dark:text-zinc-500" />
+        </div>
+      </div>
+    </Dropdown>
+  );
+}
