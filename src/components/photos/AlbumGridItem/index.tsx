a93495ee15541c5abec76499f3d1b6d542b9d051
Aleksei Maslakov <lesha.maslakov@gmail.com>
2024-04-25 00:42:40 +0300

    Support selection for `AlbumGridItem`

--- src/components/photos/AlbumGridItem/index.tsx
+++ src/components/photos/AlbumGridItem/index.tsx
@@ -10,6 +10,8 @@ import * as icons from 'icons';
 
 import { ItemRendererProps } from 'components/ui/VGrid';
 
+import { useSelection } from 'components/SelectionProvider';
+
 import { ItemDataProps } from '../AlbumGridView';
 
 interface GridItemProps {
@@ -18,15 +20,37 @@ interface GridItemProps {
 }
 
 function GridItem({ album, width }: GridItemProps) {
+  const { select, toggleSelection, isSelected } = useSelection();
+  const selected = isSelected(album.id);
+
+  const onSelect = (event: React.MouseEvent<HTMLSpanElement>) => {
+    event.stopPropagation();
+
+    if (event.metaKey) {
+      toggleSelection(album.id);
+    } else {
+      select(album.id);
+    }
+  };
+
   return (
     <div className="group flex h-full flex-col items-center justify-center">
       {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
-      <div
-        className="flex h-full w-full items-center justify-center rounded-xl bg-gray-100 dark:bg-zinc-700"
-        style={{ width: width - 24, height: width - 24 }}
+      <span
+        className={`relative ${
+          selected
+            ? 'm-2 rounded-lg ring-2 ring-blue-600 ring-offset-2 ring-offset-white dark:ring-indigo-500 dark:ring-offset-4 dark:ring-offset-zinc-800'
+            : ''
+        }`}
+        onClick={onSelect}
       >
-        <icons.PhotographOutlined className="h-12 w-12 dark:text-zinc-400" />
-      </div>
+        <div
+          className="flex h-full w-full items-center justify-center rounded-xl bg-gray-100 dark:bg-zinc-700"
+          style={{ width: width - (selected ? 40 : 24), height: width - (selected ? 40 : 24) }}
+        >
+          <icons.PhotographOutlined className="h-12 w-12 dark:text-zinc-400" />
+        </div>
+      </span>
       <div className="pt-1 text-center">
         <p>{album.title}</p>
         <p className="text-xs text-gray-500 dark:text-zinc-400">{0}</p>
