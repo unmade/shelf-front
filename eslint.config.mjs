fa9b029c803c697cd48df2e44d8f486994b032f6
Aleksei Maslakov <lesha.maslakov@gmail.com>
2025-07-29 21:14:39 +0200

    Fix `eslint` config

---
+++ eslint.config.mjs
@@ -0,0 +1,70 @@
+import js from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier/flat';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import globals from 'globals';
+import { config, configs } from 'typescript-eslint';
+
+const eslintConfig = config(
+  {
+    name: 'global-ignores',
+    ignores: [
+      '**/*.snap',
+      '**/dist/',
+      '**/node_modules/',
+      '**/.yalc/',
+      '**/build/',
+      '**/temp/',
+      '**/.temp/',
+      '**/.tmp/',
+      '**/.yarn/',
+      '**/coverage/',
+    ],
+  },
+  {
+    name: `${js.meta.name}/recommended`,
+    ...js.configs.recommended,
+  },
+  // configs.strictTypeChecked,
+  configs.stylisticTypeChecked,
+  configs.recommended,
+  {
+    name: 'eslint-plugin-react/jsx-runtime',
+    ...reactPlugin.configs.flat['jsx-runtime'],
+  },
+  reactHooksPlugin.configs['recommended-latest'],
+  jsxA11y.flatConfigs.recommended,
+  {
+    name: 'main',
+    linterOptions: {
+      reportUnusedDisableDirectives: 2,
+    },
+    languageOptions: {
+      ecmaVersion: 12,
+      globals: globals.browser,
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ['*.js', '*.mjs', '*.mts'],
+        },
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      'no-undef': [0],
+      '@typescript-eslint/consistent-type-definitions': 'error',
+      '@typescript-eslint/consistent-type-imports': [
+        2,
+        {
+          prefer: 'type-imports',
+          fixStyle: 'separate-type-imports',
+          disallowTypeAnnotations: true,
+        },
+      ],
+    },
+  },
+
+  prettierConfig,
+);
+
+export default eslintConfig;
