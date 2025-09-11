a1dd344ea9dcef569ac98440e489d6ce8536fc94
Aleksei Maslakov <lesha.maslakov@gmail.com>
2025-09-11 21:38:15 +0200

    Add hover for the `CurrentAccount`

--- src/components/AppSidebar/CurrentAccount/index.tsx
+++ src/components/AppSidebar/CurrentAccount/index.tsx
@@ -21,30 +21,34 @@ export default function CurrentAccount() {
     return null;
   }
 
-  const { display_name: fullName, username } = account;
+  const { display_name: fullName, email, username } = account;
   const displayName = fullName || username;
 
   return (
     <UIPopover>
-      <UIPopoverButton className="w-full focus:outline-none">
-        <div className="flex flex-row items-center">
-          <Avatar username={displayName} className="size-10 shrink-0" />
-          <div className="ml-2 flex min-w-0 flex-1 flex-col text-left">
-            <div className="truncate text-sm font-semibold text-gray-700 dark:text-zinc-400">
-              {displayName}
-            </div>
-          </div>
-          <div>
-            <icons.ChevronUpIcon className="size-5 text-gray-500 dark:text-zinc-500" />
-          </div>
+      <UIPopoverButton className="flex w-full items-center justify-between rounded-lg px-2 py-1.5 focus:outline-none data-hover:bg-gray-950/5 dark:data-hover:dark:bg-zinc-50/5">
+        <div className="flex min-w-0 items-center gap-3 text-left">
+          <Avatar username={displayName} className="size-10" />
+          <span className="min-w-0">
+            <span className="block truncate text-base/6 font-medium text-gray-950 sm:text-sm/5 dark:text-white">
+              {fullName || `@${username}`}
+            </span>
+            {displayName && (
+              <span className="block truncate text-sm/6 font-normal text-gray-500 sm:text-xs/5 dark:text-zinc-400">
+                {email ?? `@${username}`}
+              </span>
+            )}
+          </span>
         </div>
+        <icons.ChevronUpIcon className="size-5 text-gray-500 sm:size-4 dark:text-zinc-400" />
       </UIPopoverButton>
 
       <UIPopoverPanel
         anchor="top start"
         transition
         className={[
-          'z-40 w-(--button-width) rounded-2xl',
+          'px-2 pt-4 pb-2',
+          'z-40 w-(--button-width) rounded-xl',
           '[--anchor-gap:4px] sm:[--anchor-gap:8px]',
           'bg-white/75 shadow backdrop-blur dark:bg-zinc-800/75',
           'ring-1 ring-zinc-950/10 dark:ring-white/10',
