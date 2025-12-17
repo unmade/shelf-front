f1acd993040cd0a246ed9db8c1e232794f1b3fc7
Aleksei Maslakov <lesha.maslakov@gmail.com>
2025-12-17 22:56:53 +0100

    Rewrite `store/authSlice` to typescript

---
+++ src/store/authSlice.ts
@@ -0,0 +1,52 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+import { type RootState } from './store';
+import { getItem } from '@/hooks/local-storage';
+
+interface State {
+  accessToken: string | null;
+  refreshToken: string | null;
+  refreshedAt: string | null;
+}
+
+const initialState: State = { accessToken: null, refreshToken: null, refreshedAt: null };
+
+const slice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    tokenRefreshed: (_, action) => {
+      const { accessToken, refreshToken } = action.payload;
+      return { accessToken, refreshToken, refreshedAt: new Date().toISOString() };
+    },
+    signedOut: () => initialState,
+  },
+});
+
+export const { tokenRefreshed, signedOut } = slice.actions;
+export default slice.reducer;
+
+export const selectAccessToken = (state: RootState) => state.auth.accessToken;
+export const selectAccessTokenRefreshedAt = (state: RootState) => state.auth.refreshedAt;
+export const selectRefreshToken = (state: RootState) => state.auth.refreshToken;
+
+const KEY = 'state.auth';
+
+export const saveAuthState = (state: RootState) => {
+  const refreshToken = selectRefreshToken(state);
+  const tokensState = JSON.stringify({ refreshToken });
+  if (tokensState !== localStorage.getItem(KEY)) {
+    localStorage.setItem(KEY, tokensState);
+  }
+};
+
+export const loadAuthState = () => {
+  const rawState = getItem<string>(KEY);
+  const state = rawState ? JSON.parse(rawState) : {};
+  return {
+    auth: {
+      ...initialState,
+      ...state,
+    },
+  };
+};
