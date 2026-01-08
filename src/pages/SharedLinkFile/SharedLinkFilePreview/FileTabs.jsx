825a242559450c1b519e0586d05e3a6f08f861f9
Aleksei Maslakov <lesha.maslakov@gmail.com>
2026-01-08 22:54:43 +0100

    Replace `Tabs` component with `shadcn` one

--- src/pages/SharedLinkFile/SharedLinkFilePreview/FileTabs.jsx
+++ src/pages/SharedLinkFile/SharedLinkFilePreview/FileTabs.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 import { useTranslation } from 'react-i18next';
 
-import { SharedLinkFileShape } from '../../../types';
+import { SharedLinkFileShape } from '@/types';
 
-import Tabs from '../../../components/ui-legacy/Tabs';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/ui/tabs';
 
-import FileTabPanels from '../../../components/FileTabPanels';
+import FileTabPanels from '@/components/FileTabPanels';
 
 function FileTabs({ file }) {
   const { t } = useTranslation('file');
@@ -18,7 +18,22 @@ function FileTabs({ file }) {
     },
   ];
 
-  return <Tabs tabs={tabs} />;
+  return (
+    <Tabs defaultValue={tabs[0].key} className="w-full">
+      <TabsList className="w-full">
+        {tabs.map(({ key, name }) => (
+          <TabsTrigger key={key} value={key}>
+            {name}
+          </TabsTrigger>
+        ))}
+      </TabsList>
+      {tabs.map(({ key, renderer }) => (
+        <TabsContent key={key} value={key}>
+          {renderer}
+        </TabsContent>
+      ))}
+    </Tabs>
+  );
 }
 
 FileTabs.propTypes = {
